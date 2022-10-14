4c07d860c8cadff53d1e6eeceb8c6cb867bd77f6
Stas <example@gmail.com>
2022-10-14 22:04:34 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+// function func(){
+
+//     try{
+//         if(Math.random()<=0.5){
+//             throw new Error('')
+//         }
+//         return 1
+//     }
+//     catch(e){
+//         return 2
+//     } finally {
+//         return 3
+//     }
+// }
+// console.log(func())
+
+
+// let obj1 = {
+//     name:"Vasya"
+// };
+// let obj2 = obj1
+// obj1 = {
+//     name: 'Petr 1'
+// }
+// console.log(obj2)
+
+
+// console.log(0.1+0.2== 0.3)
+
+
+
+let name = "arhangle"
+
+function func(){
+    let name = "drown"
+}
+
+name = "peak";
+
+func()
+
+console.log(name)
+
+
+console.log(typeof null)
+
+console.log(null instanceof Object)
